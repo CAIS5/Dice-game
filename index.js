f8c7ec17fe43c34ed621e9a3547f556191c6770e
Cais <62311487+s3nzen@users.noreply.github.com>
2020-11-12 00:01:31 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,63 +1,63 @@
 const colors = document.querySelector(".colors");
 const diceFaces = Array.from(document.querySelectorAll(".dice"));
-const stageOneBtn = document.querySelectorAll('.btn-stage-one button');
-const stageTwoBtn = document.querySelectorAll('.btn-container button');
+const stageOneBtn = document.querySelectorAll(".btn-stage-one button");
+const stageTwoBtn = document.querySelectorAll(".btn-container button");
 
-let p1_score = 0 ;
+let p1_score = 0;
 let p2_score = 0;
 let p2Turn = false;
 init();
-function init(){
+function init() {
+  // INITIAL STAGE BUTTON
+  // 1 - Select all the buttons ( onePlayer, twoPlayer, help  and closeModal button) from stage one.
+  // 2 - Assigning task according to the function of button for.
+  stageOneBtn.forEach((b) => {
+    b.addEventListener("click", (e) => {
+      if (e.target.classList[1] == "btn--help") {
+        document.querySelector(".overlay").style.display = "block";
+      }
 
-    // INITIAL STAGE BUTTON
-    //  1 - Select all the buttons ( onePlayer, twoPlayer, help  and closeModal button) from stage one.
-    //  2 - Assigning task according to the function of button for.
-    stageOneBtn.forEach(b => {
-        b.addEventListener('click', e => {
-            if(e.target.classList[1] == "btn--help"){
-                document.querySelector(".overlay").style.display = "block";
-            } 
+      if (e.target.classList[1] == "btn--close") {
+        document.querySelector(".overlay").style.display = "none";
+      }
 
-            if(e.target.classList[1] == "btn--close"){
-                document.querySelector(".overlay").style.display = "none";
-            } 
-
-            if(e.target.classList[1] == "btn--onePlayer" || e.target.classList[1] == "btn--twoPlayers"){
-                document.querySelector(".stage-one").style.display = "none";
-                document.querySelector(".stage-two").style.display = "block";                 
-            } 
-
-        })
+      if (
+        e.target.classList[1] == "btn--onePlayer" ||
+        e.target.classList[1] == "btn--twoPlayers"
+      ) {
+        document.querySelector(".stage-one").style.display = "none";
+        document.querySelector(".stage-two").style.display = "block";
+      }
     });
+  });
 
-    stageTwoBtn.forEach(b => {
-        b.addEventListener('click' , e => {
-            if (e.target.classList[1] == "btn-back"){
-                document.querySelector(".stage-one").style.display = "block";
-                document.querySelector(".stage-two").style.display = "none";
-                p2_score = 0;
-                p1_score = 0; 
-                document.querySelector('#player-02--score').textContent = p2_score;
-                document.querySelector('#player-01--score').textContent = p1_score;
-            } else {
-                rollDice();
-                let i = addDiceVisibility();
-                let val = parseInt(diceFaces[i].attributes.value.textContent);
-                console.log(val);
-                if(p2Turn){
-                    p2_score += val;
-                    document.querySelector('#player-02--score').textContent = p2_score;
-                    p2Turn = false;
-                } else {
-                    p1_score += val;
-                    document.querySelector('#player-01--score').textContent = p1_score;
-                    p2Turn = true;
-                }
-            }
-        })
-    })
+  stageTwoBtn.forEach((b) => {
+    b.addEventListener("click", (e) => {
+      if (e.target.classList[1] == "btn-back") {
+        document.querySelector(".stage-one").style.display = "block";
+        document.querySelector(".stage-two").style.display = "none";
+        p2_score = 0;
+        p1_score = 0;
+        document.querySelector("#player-02--score").textContent = p2_score;
+        document.querySelector("#player-01--score").textContent = p1_score;
+      } else {
+        rollDice();
+        let i = addDiceVisibility();
+        let val = parseInt(diceFaces[i].attributes.value.textContent);
+        // console.log(val);
+        if (p2Turn) {
+          p2_score += val;
+          document.querySelector("#player-02--score").textContent = p2_score;
+          p2Turn = false;
+        } else {
+          p1_score += val;
+          document.querySelector("#player-01--score").textContent = p1_score;
+          p2Turn = true;
+        }
+      }
+    });
+  });
 }
-
 
 // CHANGING BACKGROUND COLOR
 colors.addEventListener("click", (event) => {
@@ -102,4 +102,3 @@ function shuffleArray(arr) {
 function randomNumber(min, max) {
   return Math.floor(Math.random() * max) + min;
 }
-
